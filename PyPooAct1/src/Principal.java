748bbc6083706e0ddd954299cb4602c48e1b90a2
FabricioLimache <felix12che@gmail.com>
2026-03-16 09:26:46 -0500

    Update Principal.java

--- PyPooAct1/src/Principal.java
+++ PyPooAct1/src/Principal.java
@@ -2,66 +2,91 @@ import java.util.*;
 
 public class Principal {
 
-    // método que muestra cualquier rectángulo con su nombre
-    public static void mostrarRectangulo(String nombre, Rectangulo r) {
-        System.out.println("Rectangulo " + nombre + " = (" + r.toString() + ")");
-    }
-
-    // retorna el rectángulo correspondiente al área de sobreposición
+    // Método para hallar el rectángulo formado por la intersección.
+    //La intersección usa el máximo de los inicios y el mínimo de los finales
+    // para retornar el rectángulo correspondiente al área de sobreposición
+    // la manera de funcionar es que la esquina inferior-izq del choque es el MAXIMO de las mínimas,
+    // y la esquina superior-der es el MINIMO de las máximas.
     public static Rectangulo rectanguloSobre(Rectangulo r1, Rectangulo r2) {
-        double xIzq = Math.max(r1.getEsquina1().getX(), r2.getEsquina1().getX());
-        double yInf = Math.max(r1.getEsquina1().getY(), r2.getEsquina1().getY());
-        double xDer = Math.min(r1.getEsquina2().getX(), r2.getEsquina2().getX());
-        double ySup = Math.min(r1.getEsquina2().getY(), r2.getEsquina2().getY());
 
-        Coordenada c1 = new Coordenada(xIzq, yInf);
-        Coordenada c2 = new Coordenada(xDer, ySup);
+        double nuevoMinX = Math.max(r1.getEsquina1().getX(), r2.getEsquina1().getX());
+        double nuevoMinY = Math.max(r1.getEsquina1().getY(), r2.getEsquina1().getY());
 
-        return new Rectangulo(c1, c2);
+        double nuevoMaxX = Math.min(r1.getEsquina2().getX(), r2.getEsquina2().getX());
+        double nuevoMaxY = Math.min(r1.getEsquina2().getY(), r2.getEsquina2().getY());
+
+        Coordenada cMin = new Coordenada(nuevoMinX, nuevoMinY);
+        Coordenada cMax = new Coordenada(nuevoMaxX, nuevoMaxY);
+
+        return new Rectangulo(cMin, cMax);
     }
 
+    public static String mostrarRectangulo(Rectangulo r) {//muestra la informacion de un triangulo
+        return r.toString();
+    }
+    
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner lector = new Scanner(System.in);
 
-        System.out.println("Ingrese una esquina del 1er rectángulo:");
-        double x1 = sc.nextDouble();
-        double y1 = sc.nextDouble();
+        // entrada de datos del rectangulo A (primer rectangulo)
+        System.out.println(">>> CONFIGURACIÓN DEL RECTÁNGULO A");
+        System.out.println("Ingrese la primera esquina:");
+        System.out.print("Punto X: ");
+        double x1A = lector.nextDouble();
+        System.out.print("Punto Y: ");
+        double y1A = lector.nextDouble();
+        Coordenada c1A = new Coordenada(x1A, y1A);
 
-        System.out.println("Ingrese la esquina opuesta del 1er rectángulo:");
-        double x2 = sc.nextDouble();
-        double y2 = sc.nextDouble();
+        System.out.println("Ingrese la esquina opuesta:");
+        System.out.print("Punto X: ");
+        double x2A = lector.nextDouble();
+        System.out.print("Punto Y: ");
+        double y2A = lector.nextDouble();
+        Coordenada c2A = new Coordenada(x2A, y2A);
 
-        System.out.println("Ingrese una esquina del 2do rectángulo:");
-        double x3 = sc.nextDouble();
-        double y3 = sc.nextDouble();
+        Rectangulo recA = new Rectangulo(c1A, c2A);
 
-        System.out.println("Ingrese la esquina opuesta del 2do rectángulo:");
-        double x4 = sc.nextDouble();
-        double y4 = sc.nextDouble();
+        // entrada de datos del rectangulo B (segundo rectangulo)
+        System.out.println("\n>>> CONFIGURACIÓN DEL RECTÁNGULO B");
+        System.out.println("Ingrese la primera esquina:");
+        System.out.print("Punto X: ");
+        double x1B = lector.nextDouble();
+        System.out.print("Punto Y: ");
+        double y1B = lector.nextDouble();
+        Coordenada c1B = new Coordenada(x1B, y1B);
 
-        Coordenada a1 = new Coordenada(x1, y1);
-        Coordenada a2 = new Coordenada(x2, y2);
-        Coordenada b1 = new Coordenada(x3, y3);
-        Coordenada b2 = new Coordenada(x4, y4);
+        System.out.println("Ingrese la esquina opuesta:");
+        System.out.print("Punto X: ");
+        double x2B = lector.nextDouble();
+        System.out.print("Punto Y: ");
+        double y2B = lector.nextDouble();
+        Coordenada c2B = new Coordenada(x2B, y2B);
 
-        Rectangulo A = new Rectangulo(a1, a2);
-        Rectangulo B = new Rectangulo(b1, b2);
+        Rectangulo recB = new Rectangulo(c1B, c2B);
 
-        System.out.println();
-        mostrarRectangulo("A", A);
-        mostrarRectangulo("B", B);
+        // Mostramos la informacion de cada rectangulo
+        //usamos la esquina inferior izquierda y la esquina superior derecha
 
-        if (Verificador.esSobrePos(A, B)) {
-            System.out.println("Rectangulos A y B se sobreponen.");
+        System.out.println("\n========================================");
+        System.out.println("Rectangulo A = " + mostrarRectangulo(recA) );
+        System.out.println("Rectangulo B = " + mostrarRectangulo(recB) );
 
-            Rectangulo sobre = rectanguloSobre(A, B);
-            System.out.println("Area de sobreposicion = " + sobre.calculoArea());
-        } else if (Verificador.esJunto(A, B)) {
-            System.out.println("Rectangulos A y B se juntan");
-        } else if (Verificador.esDisJunto(A, B)) {
-            System.out.println("Rectangulos A y B son disjuntos");
+        // verificamos los casos
+        if (Verificador.SobrePuestos(recA, recB)) {
+            System.out.println("Los rectangulos A y B se sobreponen.");
+
+            // calculamos el area de la sobreposicion
+            Rectangulo areaChoque = rectanguloSobre(recA, recB);
+            System.out.println("Area de sobreposicion = " + areaChoque.calculoArea());
+
+        } else if (Verificador.Juntos(recA, recB)) {
+            System.out.println("Los rectangulos A y B se juntan.");
+
+        } else if (Verificador.Disjuntos(recA, recB)) {
+            System.out.println("Los rectangulos A y B son disjuntos.");
         }
 
-        sc.close();
+
     }
+
 }
