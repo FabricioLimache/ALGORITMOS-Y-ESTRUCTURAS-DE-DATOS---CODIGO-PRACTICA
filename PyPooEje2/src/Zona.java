d99d2f9122c787a08adcdcaddd543129a1779608
samuelSC97 <samuel.sucasaire97@gmail.com>
2026-03-16 01:13:47 -0500

    Correccion de nombre de proyectos

---
+++ PyPooEje2/src/Zona.java
@@ -0,0 +1,26 @@
+
+public class Zona {
+    private String tipoMineral;
+    private double cantidad;
+    private double pureza;
+
+    public Zona(String tipoMineral, double cantidad, double pureza) {
+        this.tipoMineral = tipoMineral;
+        this.cantidad = cantidad;
+        this.pureza = pureza;
+    }
+
+    // El valor económico se calcula multiplicando cantidad por pureza
+    public double getValorEconomico() {
+        return cantidad * pureza;
+    }
+
+    public String getTipoMineral() {
+        return tipoMineral;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + tipoMineral + ", cantidad: " + cantidad + ", pureza: " + pureza + "]";
+    }
+}
