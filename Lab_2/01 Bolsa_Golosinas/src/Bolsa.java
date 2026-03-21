1a6c7a0db6c963e28dd4c7b5c74e6c778ea7bb6f
FabricioLimache <felix12che@gmail.com>
2026-03-21 06:27:18 -0500

    Update Bolsa.java

--- Lab_2/01 Bolsa_Golosinas/src/Bolsa.java
+++ Lab_2/01 Bolsa_Golosinas/src/Bolsa.java
@@ -1,2 +1,7 @@
 public class Bolsa {
+    
+    public static void main(String[] args) {
+        // Imprime un mensaje directo en la consola
+        System.out.println("La clase Bolsa se ha ejecutado correctamente.");
+    }
 }
