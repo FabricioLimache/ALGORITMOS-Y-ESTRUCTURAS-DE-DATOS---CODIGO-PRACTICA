91b80fd6d6ee6109ce2caaf321d783719732a3e6
FabricioLimache <felix12che@gmail.com>
2026-03-21 09:16:31 -0500

    Update Chocolatina.java

--- Lab_2/01 Bolsa_Golosinas/src/Chocolatina.java
+++ Lab_2/01 Bolsa_Golosinas/src/Chocolatina.java
@@ -1,1 +1,12 @@
+public class Chocolatina {
+    private String marca;
 
+    // Solo necesitamos la marca para diferenciar estas piezas
+    public Chocolatina(String marca) {
+        this.marca = marca;
+    }
+
+    // getters y setters
+    public String getMarca() { return marca; }
+    public void setMarca(String marca) { this.marca = marca; }
+}
