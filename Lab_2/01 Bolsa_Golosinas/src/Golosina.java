04631a36f2f854aebdfd636bc19b14bfbca1055f
FabricioLimache <felix12che@gmail.com>
2026-03-21 09:12:31 -0500

    Update Golosina.java

--- Lab_2/01 Bolsa_Golosinas/src/Golosina.java
+++ Lab_2/01 Bolsa_Golosinas/src/Golosina.java
@@ -1,1 +1,16 @@
+public class Golosina {
+    private String nombre;
+    private double peso;
 
+    // Constructor para darle identidad a cada golosina con su nombre y peso
+    public Golosina(String nombre, double peso) {
+        this.nombre = nombre;
+        this.peso = peso;
+    }
+
+    // getters y setters
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public double getPeso() { return peso; }
+    public void setPeso(double peso) { this.peso = peso; }
+}
