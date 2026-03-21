7b70013793d09af228d7de30f335854c54550a32
FabricioLimache <felix12che@gmail.com>
2026-03-21 09:14:26 -0500

    Delete Lab_2/01 Bolsa_Golosinas/src/Bolsa.java

--- Lab_2/01 Bolsa_Golosinas/src/Bolsa.java
+++
@@ -1,7 +0,0 @@
-public class Bolsa {
-    
-    public static void main(String[] args) {
-        // Imprime un mensaje directo en la consola
-        System.out.println("La clase Bolsa se ha ejecutado correctamente.");
-    }
-}
