c2a4e37f25b7aa4d8495924dd6e9118fc329c544
FabricioLimache <felix12che@gmail.com>
2026-03-21 09:17:28 -0500

    Update DemoMetodoGenerico.java

--- Lab_2/01 Bolsa_Golosinas/src/DemoMetodoGenerico.java
+++ Lab_2/01 Bolsa_Golosinas/src/DemoMetodoGenerico.java
@@ -1,1 +1,19 @@
+public class DemoMetodoGenerico {
 
+    // compara dos arreglos del mismo tipo T para ver si son idénticos
+    public static <T extends Comparable<T>> boolean igualArrays(T[] x, T[] y) {
+        // Si no miden lo mismo, es imposible que sean iguales
+        if (x.length != y.length)
+            return false;
+
+        for (int i = 0; i < x.length; i++) {
+            /* Usamos la función equals aquí para comparar el contenido real de los objetos
+              Si un elemento no coincide, devolvemos false de inmediato.
+            */
+            if (!x[i].equals(y[i]))
+                return false;
+        }
+        // Si el for termina, significa que todo es equivalente
+        return true;
+    }
+}
