0c185bd1fe8f15d9d7262c06abd2b9d0758daf72
ChristianCesarAQ <christian.aguirre.ucsm.edu.pe>
2026-03-22 10:09:02 -0500

    Ejercicio 4: metodos search(), delete() y toString en clase Cajoneria

--- Lab_2/02 CajoneriaGolosinas/src/Cajoneria.java
+++ Lab_2/02 CajoneriaGolosinas/src/Cajoneria.java
@@ -2,7 +2,8 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 /*contenedor genérico de Cajas
- Cada caja tiene un color y almacena un objeto de tipo T */
+ en lugar de almcnar objtos, almacena objtos Caja<T> dnde cada caja contiene
+ un obj de tipo T*/
 
 public class Cajoneria<T> implements Iterable<Caja<T>> {
     private ArrayList<Caja<T>> cajas = new ArrayList<>();
@@ -18,6 +19,45 @@ public class Cajoneria<T> implements Iterable<Caja<T>> {
         } else {
             throw new RuntimeException("no caben más cajas");
         }
+    }
+    
+    //el metodo equals ya compara por nombre y peso ya que en el 
+    //ejecicio 01 ya se modifico el equals() de Golosina para que la comparacion sea por ambos atributos
+    
+    ////metodo search: 
+    public String search(T elemento){
+    	for(int i=0; i<cajas.size(); i++)
+    		if(cajas.get(i).getObjeto().equals(elemento))
+    			return "Posicion: " + (i+1) +", Color: " + cajas.get(i).getColor();    
+    	return null;
+    }
+    
+    //metodo delete
+    public T delete(T elemento) {
+    	for(int i=0; i<cajas.size(); i++ ) {
+    		if(cajas.get(i).getObjeto().equals(elemento)) {
+    			T objEliminado = cajas.get(i).getObjeto();
+    			cajas.remove(i);
+    			return objEliminado;
+    					
+    		}
+    	}
+    	return null;
+    }
+    
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("%-10s %-15s %s%n", "Posicion", "Color Caja", "Objeto"));
+        sb.append("-".repeat(60)).append("\n");
+        for (int i = 0; i < cajas.size(); i++) {
+            sb.append(String.format("%-10d %-15s %s%n",
+                (i + 1),
+                cajas.get(i).getColor(),
+                cajas.get(i).getObjeto()
+            ));
+        }
+        return sb.toString();
     }
 
     @Override
