d2ddb2937a759aa29d1c18666b9902b59acaa95b
FabricioLimache <felix12che@gmail.com>
2026-05-07 10:26:51 +0000

    Update InvertirPila.java

--- Lab6/src/Ejercicio2ColaArreglo/InvertirPila.java
+++ Lab6/src/Ejercicio2ColaArreglo/InvertirPila.java
@@ -1,1 +1,16 @@
+public class InvertirPila {
+    // Método estático que realiza la inversión física de los nodos
+    public static <T> void invertir(StackLink<T> pila) {
+        ColaEnlazada<T> colaAux = new ColaEnlazada<>();
 
+        // Transferencia de Pila a Cola (invierte el orden de salida)
+        while (!pila.isEmpty()) {
+            colaAux.encolar(pila.pop());
+        }
+
+        // Transferencia de Cola a Pila (restablece el nuevo orden inverso)
+        while (!colaAux.estaVacia()) {
+            pila.push(colaAux.desencolar());
+        }
+    }
+}
